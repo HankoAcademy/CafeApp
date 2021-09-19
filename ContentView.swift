9935be125e30c4baa7d7d6d3899c4ba54bd69ca6
Yunseo Lucas Lee <yunseolee.0503@gmail.com>
2021-09-19 16:37:35 -0700

    Added View files and setup UITableView for testing

---
+++ ContentView.swift
@@ -0,0 +1,8 @@
+//
+//  ContentView.swift
+//  CafeApp
+//
+//  Created by Yunseo Lee on 9/19/21.
+//
+
+import Foundation
---
+++ ProductDetailTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductDetailTableViewCell.swift
+//  CafeApp
+//
+//  Created by Yunseo Lee on 9/19/21.
+//
+
+import UIKit
+
+class ProductDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
