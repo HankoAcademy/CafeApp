1a1dfca786fe72c41b159348c7e3536d2b5bc985
Christian Hawkes <chawkes77@gmail.com>
2021-09-23 18:41:45 -0400

    Add logo header view

---
+++ CafeApp/CafeLogoView.swift
@@ -0,0 +1,73 @@
+//
+//  CafeLogoView.swift
+//  CafeApp
+//
+//  Created by Christian on 9/22/21.
+//
+
+import Foundation
+import UIKit
+
+class CafeLogoView: UIStackView {
+
+    // MARK: - UI Properties
+
+    let headerImageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "logo_coffee")
+        image.image?.withRenderingMode(.alwaysTemplate)
+        image.tintColor = .white
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
+    let headerLabelView: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 30, weight: .bold)
+        label.textColor = .darkGray
+        label.textAlignment = .center
+        label.text = "Hanko Cafe"
+        return label
+    }()
+
+    // MARK - Initializers
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+        
+        setUpUI()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpUI() {
+        axis = .horizontal
+        spacing = 20
+        distribution = .fillProportionally
+        
+        addHeaderImage()
+        addHeaderLabel()
+    }
+    
+    private func addHeaderImage() {
+        addArrangedSubview(headerImageView)
+        
+        NSLayoutConstraint.activate([
+            headerImageView.heightAnchor.constraint(equalToConstant: 150),
+            headerImageView.widthAnchor.constraint(equalTo: headerImageView.heightAnchor)
+        ])
+    }
+    
+    private func addHeaderLabel() {
+        addArrangedSubview(headerLabelView)
+    }
+    
+}
+
+
--- CafeApp/ContentView.swift
+++ CafeApp/ContentView.swift
@@ -19,6 +19,12 @@ class ContentView: UIView {
         return tableView
     }()
     
+    let cafeLogoView: CafeLogoView = {
+        let cafeLogoView = CafeLogoView()
+        cafeLogoView.translatesAutoresizingMaskIntoConstraints = false
+        return cafeLogoView
+    }()
+    
     // MARK: - Initializers
     
     override init(frame: CGRect) {
@@ -36,11 +42,28 @@ class ContentView: UIView {
     // MARK: - Set Up UI
     
     private func setUpUI() {
+        
+        addLogoStackView()
+        addTableView()
+    }
+    
+    private func addLogoStackView() {
+        addSubview(cafeLogoView)
+        
+        NSLayoutConstraint.activate([
+            cafeLogoView.topAnchor.constraint(equalTo: topAnchor, constant: 20),
+            cafeLogoView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            cafeLogoView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            cafeLogoView.heightAnchor.constraint(equalToConstant: 100)
+        ])
+    }
+    
+    private func addTableView() {
         addSubview(tableView)
         
         NSLayoutConstraint.activate([
             tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            tableView.heightAnchor.constraint(equalTo: heightAnchor),
+            tableView.topAnchor.constraint(equalTo: cafeLogoView.bottomAnchor, constant: 20),
             tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
             tableView.trailingAnchor.constraint(equalTo: trailingAnchor)
         ])
