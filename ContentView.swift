9cdd8a9a6974cdb2f2b68631550fda7d3dc3fd1d
Sotiri Kolvani <sotiri.kolvani@gmail.com>
2021-09-28 12:25:51 -0400

    Cafe App showing TableView with Title, Image, Description, and Price

--- ContentView.swift
+++ ContentView.swift
@@ -6,3 +6,61 @@
 //
 
 import Foundation
+import UIKit
+
+
+class ContentView: UIView {
+
+    // MARK: - UI Properties
+    
+    var productTableHeaderView: ProductTableHeaderView = {
+           let headerView = ProductTableHeaderView()
+           headerView.translatesAutoresizingMaskIntoConstraints = false
+           return headerView
+       }()
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = UIColor(named: "Cream")
+        tableView.register(ProductDetailTableViewCell.self, forCellReuseIdentifier: "ProductDetailTableViewCell")
+        tableView.register(ProductTableHeaderView.self, forHeaderFooterViewReuseIdentifier: "ProductTableHeaderView")
+        return tableView
+    }()
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+        
+        
+        setUpViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UI Setup
+    
+    private func setUpViews() {
+        
+        addSubview(tableView)
+        addSubview(productTableHeaderView)
+        
+        NSLayoutConstraint.activate([
+            
+            productTableHeaderView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            productTableHeaderView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            productTableHeaderView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+} // End of Class
--- ProductDetailTableViewCell.swift
+++ ProductDetailTableViewCell.swift
@@ -6,3 +6,116 @@
 //
 
 import Foundation
+import UIKit
+
+
+class ProductDetailTableViewCell: UITableViewCell {
+    
+    // MARK: - UI Properties
+        
+        private var leftSideStackView: UIStackView = {
+            let stackView = UIStackView()
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            stackView.axis = .horizontal
+            stackView.distribution = .fillProportionally
+            stackView.spacing = 10
+            return stackView
+        }()
+    
+        private var textStackView: UIStackView = {
+            let stackView = UIStackView()
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            stackView.axis = .vertical
+            stackView.distribution = .fillProportionally
+            stackView.alignment = .leading
+            stackView.spacing = 2.5
+            return stackView
+        }()
+    
+        private var itemImageView: UIImageView = {
+            let imageView = UIImageView()
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            imageView.contentMode = .scaleAspectFit
+            return imageView
+       }()
+        
+        private var itemTitleLabel: UILabel = {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+            label.textColor = .black
+            label.textAlignment = .left
+            return label
+        }()
+    
+        private var itemDescriptionLabel: UILabel = {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+            label.textColor = .lightGray
+            label.textAlignment = .left
+            return label
+    }()
+        
+        private var itemPriceLabel: UILabel = {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+            label.textColor = .black
+            label.textAlignment = .right
+            return label
+        }()
+        
+        // MARK: - Initializers
+        
+        override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+            super.init(style: style, reuseIdentifier: reuseIdentifier)
+            
+            setUpUI()
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        // MARK: - UI Setup
+        
+        private func setUpUI() {
+            
+            contentView.backgroundColor = UIColor(named: "Cream")
+            
+            textStackView.addArrangedSubview(itemTitleLabel)
+            textStackView.addArrangedSubview(itemDescriptionLabel)
+                    
+            leftSideStackView.addArrangedSubview(itemImageView)
+            leftSideStackView.addArrangedSubview(textStackView)
+                    
+            contentView.addSubview(leftSideStackView)
+            contentView.addSubview(itemPriceLabel)
+            
+            NSLayoutConstraint.activate([
+                itemImageView.widthAnchor.constraint(equalToConstant: 25),
+                    
+                leftSideStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 24),
+                leftSideStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+                leftSideStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16),
+                leftSideStackView.trailingAnchor.constraint(equalTo: itemPriceLabel.leadingAnchor, constant: -8),
+                    
+                itemPriceLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+                itemPriceLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16)
+            ])
+            
+                itemPriceLabel.setContentHuggingPriority(.required, for: .horizontal)
+                itemPriceLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
+        }
+        
+        func update(itemTitle: String, itemPrice: Double, itemImage: String, itemDescription: String) {
+            
+            itemTitleLabel.text = itemTitle
+            itemPriceLabel.text = String(format: "$%.02f", itemPrice)
+            itemImageView.image = UIImage(named: itemImage)
+            itemDescriptionLabel.text = itemDescription
+        }
+    
+    
+} // End of Class
