d460fb2ff2f8b9b7b3517d71c697ac5a744fdc1a
Yunseo Lucas Lee <yunseolee.0503@gmail.com>
2021-09-28 13:10:47 -0700

    Menu table with header now properly in function

--- MenuItemView.swift
+++ MenuItemView.swift
@@ -8,13 +8,83 @@
 import UIKit
 
 class MenuItemView: UITableViewCell {
+    
+    // MARK: - Parameter Properties
+    
+    var imageName: String = "" {
+            didSet {
+                itemImageView.image = UIImage(named: imageName)
+            }
+        }
+        
+        var itemName: String = "" {
+            didSet {
+                itemTitleLabel.text = itemName
+            }
+        }
+        
+        var itemDescription: String = "" {
+            didSet {
+                itemDescriptionLabel.text = itemDescription
+            }
+        }
+        
+        var itemPrice: Double = 0 {
+            didSet {
+                itemPriceLabel.text = String(format: "$%.02f", itemPrice)
+            }
+        }
 
     // MARK: - UI Properties
+    
+    private var sideStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.alignment = .leading
+        return stackView
+    }()
+    
+    private var itemImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image?.withRenderingMode(.alwaysTemplate)
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
     
     private var stackView: UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fillProportionally
+        stackView.alignment = .leading
         return stackView
+    }()
+    
+    private var itemTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        return label
+    }()
+    
+    private var itemDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.font = UIFont.systemFont(ofSize: 13, weight: .medium)
+        return label
+    }()
+
+    private var itemPriceLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .right
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        return label
     }()
     
     // MARK: - Initializers
@@ -28,10 +98,50 @@ class MenuItemView: UITableViewCell {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+        
+        
     }
     
+    // MARK: - UI Setup
+    
     private func setUpUI(){
+        contentView.backgroundColor = UIColor(named: "Cream")
         
+        setUpImageView()
+        setUpTextLabel()
+    }
+    
+    private func setUpImageView(){
         
+        sideStackView.addArrangedSubview(itemImageView)
+        contentView.addSubview(sideStackView)
+        
+        NSLayoutConstraint.activate([
+            sideStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 24),
+            sideStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+            sideStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
+        ])
+    }
+    
+    private func setUpTextLabel(){
+        
+        stackView.addArrangedSubview(itemTitleLabel)
+        stackView.addArrangedSubview(itemPriceLabel)
+        contentView.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            itemTitleLabel.leadingAnchor.constraint(equalTo: sideStackView.trailingAnchor, constant: 16),
+            itemImageView.widthAnchor.constraint(equalToConstant: 30),
+            itemPriceLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            itemPriceLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16)
+        ])
+
+    }
+    
+    func update(itemImage: UIImage, itemTitle: String, itemPrice: Double) {
+        
+        itemImageView.image = itemImage
+        itemTitleLabel.text = itemTitle
+        itemPriceLabel.text = String(format: "$%.02f", itemPrice)
     }
 }
--- TableHeaderView.swift
+++ TableHeaderView.swift
@@ -5,4 +5,79 @@
 //  Created by Yunseo Lee on 9/26/21.
 //
 
-import Foundation
+import UIKit
+
+class TableHeaderView: UITableViewHeaderFooterView {
+    
+    // MARK: - Class Properties
+    
+    var headerName: String = "" {
+        didSet {
+            let attributedString = NSMutableAttributedString(string: headerName)
+            attributedString.addAttribute(NSAttributedString.Key.kern, value: 14, range: NSRange(location: 0, length: attributedString.length))
+            headerLabel.attributedText = attributedString
+        }
+    }
+    
+    //ADD Filter and Sort here
+    
+    // MARK: - UI Properties
+    
+    private var headerLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private var bottomLineView: UIView = {
+        let line = UIView()
+        line.backgroundColor = UIColor(named: "Navy")
+        line.translatesAutoresizingMaskIntoConstraints = false
+        return line
+    }()
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.isHidden = true
+        return stackView
+    }()
+    
+    // MARK: - Initializers
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        
+        setUpUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setUpUI()
+    }
+    
+    private func setUpUI(){
+        
+        contentView.addSubview(headerLabel)
+        contentView.addSubview(bottomLineView)
+        
+        
+        NSLayoutConstraint.activate([
+            headerLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 24),
+            headerLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 24),
+            headerLabel.bottomAnchor.constraint(equalTo: bottomLineView.topAnchor, constant: -16),
+            
+            bottomLineView.leadingAnchor.constraint(equalTo: headerLabel.leadingAnchor),
+            bottomLineView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            bottomLineView.heightAnchor.constraint(equalToConstant: 1),
+            bottomLineView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+            
+        ])
+        
+    }
+    
+    
+}
